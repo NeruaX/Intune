2170c3782e1f0053b126b37f0eee7f7cc05082f6
NeruaX <128629116+NeruaX@users.noreply.github.com>
2023-03-27 17:19:54 -0700

    Add files via upload

--- GoogleChromeInstall/GoogleChrome.ps1
+++ GoogleChromeInstall/GoogleChrome.ps1
@@ -11,6 +11,8 @@ Import-Module Microsoft.PowerShell.Utility
     Requests latest msi installer for google chrome
     Runs silent install
 
+    This can be ran as a script or wraped into a .intunewin file
+
     win32 wrap
         Install as: system
         Install Command: powershell.exe -ExecutionPolicy Bypass -NoLogo -noninteractive -File GoogleChrome.ps1
@@ -18,11 +20,17 @@ Import-Module Microsoft.PowerShell.Utility
             FIle Path: C:\Program Files\Google\Chrome\Application\
             File: Chrome.exe
             File or folder exists
+    NOTE: this only detects 64 bit. may be better to use
+    C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk
     
     #####################
     #       TODO        #
     ##################### 
-    Done as of now
+    failure checks
+    Better detection for win32 wrap
+    Test 64bit switch required
+    Implement intune return codes
+    Implement Event Viewer logging
 #>
 
 
@@ -32,12 +40,26 @@ $Path = $IntunePath+"Temp\"
 $Installer = "GoogleChrome.msi"
 $Parameters = "/qn /norestart"
 
+#No Config Below This Point Needed
+
+#Check 64 Bit Script
+#$env:PROCESSOR_ARCHITEW6432 Returns AMD64 on 32 bit Powershell and blank on 64 bit if system is 64bit
+if($env:PROCESSOR_ARCHITEW6432-eq "AMD64") {
+    #Run PS command from 64 bit powershell. on 32 bit powershell sysnative returns path
+    &"$ENV:WINDIR\SysNative\WindowsPowershell\v1.0\PowerShell.exe" -File $PSCOMMANDPATH
+    #Exit after re-running full script in 64 bit subset
+    Exit
+}
+
+$64URL = "http://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi"
+$32URL = "http://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
+
 #Detets OS 64 bit or 32 bit to provide correct URL
 $OSArchitecture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
 if($OSArchitecture -eq "64-bit") {
-    $URL = "http://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise64.msi"
+    $URL = $64URL
 } else {
-    $URL = "http://dl.google.com/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
+    $URL = $32URL
 }
 
 #Creates path if not already there
